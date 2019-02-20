52f67a51c4897b9bc5ad65d8f375bb7cb1d2c23c
Tonterias <ecorreos@htomail.com>
2019-02-20 11:23:19 +0100

    Tag Topic fix commit

--- src/main/webapp/app/entities/tag/tag-detail.component.ts
+++ src/main/webapp/app/entities/tag/tag-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { JhiDataUtils } from 'ng-jhipster';
+import { Subscription } from 'rxjs';
 
 import { ITag } from 'app/shared/model/tag.model';
 
@@ -10,7 +12,20 @@ import { ITag } from 'app/shared/model/tag.model';
 export class TagDetailComponent implements OnInit {
     tag: ITag;
 
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    error: any;
+    success: any;
+    eventSubscriber: Subscription;
+    routeData: any;
+    links: any;
+    totalItems: any;
+    itemsPerPage: any;
+    page: any = 1;
+    predicate: any = 'id';
+    previousPage: any = 0;
+    reverse: any = 'asc';
+    id: any;
+
+    constructor(protected dataUtils: JhiDataUtils, protected activatedRoute: ActivatedRoute) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ tag }) => {
@@ -20,5 +35,22 @@ export class TagDetailComponent implements OnInit {
 
     previousState() {
         window.history.back();
+    }
+
+    openFile(contentType, field) {
+        return this.dataUtils.openFile(contentType, field);
+    }
+
+    loadPage(page) {
+        this.previousPage = page;
+        this.page = page;
+    }
+
+    byteSize(field) {
+        return this.dataUtils.byteSize(field);
+    }
+
+    reload() {
+        window.location.reload();
     }
 }
--- src/main/webapp/app/entities/topic/topic-detail.component.ts
+++ src/main/webapp/app/entities/topic/topic-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { JhiDataUtils } from 'ng-jhipster';
+import { Subscription } from 'rxjs';
 
 import { ITopic } from 'app/shared/model/topic.model';
 
@@ -10,7 +12,20 @@ import { ITopic } from 'app/shared/model/topic.model';
 export class TopicDetailComponent implements OnInit {
     topic: ITopic;
 
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    error: any;
+    success: any;
+    eventSubscriber: Subscription;
+    routeData: any;
+    links: any;
+    totalItems: any;
+    itemsPerPage: any;
+    page: any = 1;
+    predicate: any = 'id';
+    previousPage: any = 0;
+    reverse: any = 'asc';
+    id: any;
+
+    constructor(protected dataUtils: JhiDataUtils, protected activatedRoute: ActivatedRoute) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ topic }) => {
@@ -21,5 +36,22 @@ export class TopicDetailComponent implements OnInit {
 
     previousState() {
         window.history.back();
+    }
+
+    openFile(contentType, field) {
+        return this.dataUtils.openFile(contentType, field);
+    }
+
+    loadPage(page) {
+        this.previousPage = page;
+        this.page = page;
+    }
+
+    byteSize(field) {
+        return this.dataUtils.byteSize(field);
+    }
+
+    reload() {
+        window.location.reload();
     }
 }
